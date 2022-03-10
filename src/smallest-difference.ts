bace393f1e32966d781257d2acb0009cb8f427ef
Brian Grosso <brian.grosso@dottid.com>
2022-03-09 23:47:24 -0500

    completed smallest-difference

--- src/smallest-difference.ts
+++ src/smallest-difference.ts
@@ -1,31 +1,45 @@
 // https://www.algoexpert.io/questions/Smallest%20Difference
 
 function smallestDifference(arrayOne: number[], arrayTwo: number[]) {
-  let indexOne = 0;
-  let indexTwo = 0;
-  let winnerOne = 0;
-  let winnerTwo = 0;
+  let i1 = 0;
+  let i2 = 0;
+  let closestPair = [arrayOne[0], arrayTwo[0]];
+  const sortedOne = arrayOne.sort((a, b) => a - b);
+  const sortedTwo = arrayTwo.sort((a, b) => a - b);
+  console.log({ sortedOne, sortedTwo });
   const diff = (ind1: number, ind2: number) => {
     if (
-      typeof arrayOne[ind1] !== "number" ||
-      typeof arrayTwo[ind2] !== "number"
+      typeof sortedOne[ind1] !== "number" ||
+      typeof sortedTwo[ind2] !== "number"
     )
       return Infinity;
-    return arrayOne[ind1] >= arrayTwo[ind2]
-      ? arrayOne[ind1] - arrayTwo[ind2]
-      : arrayTwo[ind2] - arrayOne[ind1];
+    return sortedOne[ind1] >= sortedTwo[ind2]
+      ? sortedOne[ind1] - sortedTwo[ind2]
+      : sortedTwo[ind2] - sortedOne[ind1];
   };
-  let minDiff = diff(winnerOne, winnerTwo);
-  while (indexOne < arrayOne.length && indexTwo < arrayTwo.length) {
-    const currentDiff = diff(indexOne, indexTwo);
-	
-	const inc1Diff = diff(indexOne+1, indexTwo);
-	const inc2Diff = diff(indexOne, indexTwo+1);
-	if (inc1Diff < currentDiff && in)
-	if (minDiff === 0) return [arrayOne[winnerOne], arrayTwo[winnerTwo]];
+  let minDiff = diff(0, 0);
+  while (i1 < sortedOne.length || i2 < sortedTwo.length) {
+    const currDiff = diff(i1, i2);
+
+    if (currDiff < minDiff) {
+      minDiff = currDiff;
+      closestPair = [sortedOne[i1], sortedTwo[i2]];
+    }
+    if (currDiff === 0) return closestPair;
+    console.log({ i1, i2, currDiff, closestPair });
+    if (sortedOne[i1] > sortedTwo[i2]) {
+      if (i2 < sortedTwo.length - 1) i2++;
+      else return closestPair;
+    } else {
+      if (i1 < sortedOne.length - 1) i1++;
+      else return closestPair;
+    }
   }
+  return closestPair;
 }
 
-const arrayOne = [-1, 5, 10, 20, 28, 3];
-const arrayTwo = [26, 134, 135, 15, 17];
-console.log(smallestDifference(arrayOne, arrayTwo));
+const test = {
+  arrayOne: [-1, 5, 10, 20, 3],
+  arrayTwo: [26, 134, 135, 15, 17],
+};
+console.log(smallestDifference(test.arrayOne, test.arrayTwo));
