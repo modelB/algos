b77819f4d5fa299cb58c9b4ecfe7a964c0b11f1b
Brian Grosso <bgro63@gmail.com>
2022-10-23 12:04:50 -0400

    bst from pre and inorder

--- src/construct-binary-tree-from-preorder-and-inorder-traversal.ts
+++ src/construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -1,16 +1,45 @@
 // https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
-// TODO: implement
 
-//prog
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
 function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
-    const root = new TreeNode(preorder[0]);
-    let preIndex = 1;
-    let inIndex = 0;
-    while (preIndex < preorder.length) {
-        while (preorder[preIndex] !== inorder[inIndex]) {
-            
-        }
+  const recurse = (
+    preIndex: number,
+    inStart: number,
+    inEnd: number
+  ): TreeNode => {
+    const val = preorder[preIndex];
+    let indexInInOrder: number | null = null;
+    for (let i = inStart; i <= inEnd; i++) {
+      if (inorder[i] === val) {
+        indexInInOrder = i;
+        break;
+      }
+    }
+    const node = new TreeNode(val);
+    if (indexInInOrder > inStart) {
+      node.left = recurse(preIndex + 1, inStart, indexInInOrder - 1);
+    }
+    if (indexInInOrder < inEnd) {
+      node.right = recurse(
+        preIndex + 1 + (indexInInOrder - inStart),
+        indexInInOrder + 1,
+        inEnd
+      );
     }
 
-    
-};
+    return node;
+  };
+  return recurse(0, 0, inorder.length - 1);
+}
+
+console.log(buildTree([3, 1, 2, 4], [1, 2, 3, 4]));
